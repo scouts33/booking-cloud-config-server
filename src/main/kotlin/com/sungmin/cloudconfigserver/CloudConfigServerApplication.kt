c332758c399296e48943fea2c823e6cafac13631
sungmin.lim <scouts33@naver.com>
2022-06-16 01:21:17 +0900

    클라우드 컨피그 서버 첫 커밋

---
+++ src/main/kotlin/com/sungmin/cloudconfigserver/CloudConfigServerApplication.kt
@@ -0,0 +1,13 @@
+package com.sungmin.cloudconfigserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
+
+@EnableConfigServer
+@SpringBootApplication
+class CloudConfigServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<CloudConfigServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/sungmin/cloudconfigserver/CloudConfigServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sungmin.cloudconfigserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CloudConfigServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
